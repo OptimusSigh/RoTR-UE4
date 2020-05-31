3c9df173ef03f763db2de0dd634b305cc292900f
jonathan <cunnew>
2020-05-31 10:54:48 +0100

    initialCommit
    start of the repo
    - commit contains a UE4 3rd person template WITH starter content
    
    -project is C++ enabled

---
+++ RoTR/Source/RoTR/RoTRGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "RoTRGameMode.h"
+#include "RoTRCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ARoTRGameMode::ARoTRGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
