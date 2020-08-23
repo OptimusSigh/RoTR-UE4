07f9b16fb1454c8f3246ec5f4f70392a3c161ddf
Jonathan <b6027956@my.shu.ac.uk>
2020-08-23 23:24:37 +0100

    oops - code compile needed
    didn't update game mode c++ to reflect file changes

--- RoTR/Source/RoTR/RoTRGameMode.cpp
+++ RoTR/Source/RoTR/RoTRGameMode.cpp
@@ -7,7 +7,7 @@
 ARoTRGameMode::ARoTRGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("Blueprint'/Game/Blueprints/Character/Player/RoTRPlayer_BP'"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("Blueprint'/Game/Blueprints/PlayerCharacter/Player/RoTRPlayer_BP'"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
